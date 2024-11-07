b94ff8ca46d50720dc017d234f7b39d4fdf66c18
radiantjade <argentum1450@gmail.com>
2024-11-07 18:29:13 +0900

    (change) hair style option response - added has_length_option field

--- app/application/services/hair_model/dto/option.py
+++ app/application/services/hair_model/dto/option.py
@@ -13,6 +13,7 @@ class HairStyleOption(BaseModel):
     title: str
     description: str
     presigned_image_url: str
+    has_length_option: bool
     order: int
 
 class HairStyleLengthOption(BaseModel):
