6e66e6adf397db7c4a2360cc89106d75af6d3716
radiantjade <argentum1450@gmail.com>
2024-11-18 18:16:40 +0900

    (test) logging access token & refresh token

--- app/application/services/user/auth.py
+++ app/application/services/user/auth.py
@@ -73,11 +73,14 @@ class UserAuthApplicationService:
         for token, user_id in all_refresh_tokens:
             print(f"\nUser ID: {user_id}")
             print(f"Refresh Token: {token}")
-        print(f"\n================================")
 
         user_id = self.redis_service.validate_refresh_token(refresh_token)
 
         access_token, new_refresh_token = self.auth_token_service.create_tokens(user_id)
+
+        print(f"access token: {access_token}")
+
+        print(f"\n================================")
 
         self.redis_service.save_refresh_token(user_id, new_refresh_token)
         self.redis_service.revoke_refresh_token(refresh_token)
