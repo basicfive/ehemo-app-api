833ac6c7fea7ef8d128ef9eceeb24c8e036ea67c
radiantjade <argentum1450@gmail.com>
2024-11-25 19:26:06 +0900

    (flag) mq request open

--- app/application/services/generation/request.py
+++ app/application/services/generation/request.py
@@ -180,16 +180,16 @@ class RequestGenerationApplicationService:
             image_generation_job_list.append(ImageGenerationJobInDB.model_validate(db_image_generation_job))
 
         # MQ 요청 보내기
-        # for idx, image_generation_job in enumerate(image_generation_job_list):
-        #     message = MQPublishMessage(
-        #         **image_generation_job.model_dump(),
-        #         image_generation_job_id=image_generation_job.id,
-        #     )
-        #     await self.rabbit_mq_service.publish(message=message, expiration_sec=message_ttl_list[idx])
-        #     self.image_generation_job_repo.update(
-        #         obj_id=image_generation_job.id,
-        #         obj_in=ImageGenerationJobUpdate(status=GenerationStatusEnum.PROCESSING)
-        #     )
+        for idx, image_generation_job in enumerate(image_generation_job_list):
+            message = MQPublishMessage(
+                **image_generation_job.model_dump(),
+                image_generation_job_id=image_generation_job.id,
+            )
+            await self.rabbit_mq_service.publish(message=message, expiration_sec=message_ttl_list[idx])
+            self.image_generation_job_repo.update(
+                obj_id=image_generation_job.id,
+                obj_in=ImageGenerationJobUpdate(status=GenerationStatusEnum.PROCESSING)
+            )
 
         message_count, consumer_count = await self.rabbit_mq_service.get_queue_info()
 
