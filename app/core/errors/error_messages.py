883222d67686e5a89662d02bcbb79a909c51a0e1
radiantjade <argentum1450@gmail.com>
2024-12-08 13:42:09 +0900

    (fix) 사용자 토큰 불충분시 에러 메시지 변경

--- app/core/errors/error_messages.py
+++ app/core/errors/error_messages.py
@@ -1,2 +1,2 @@
-USER_HAS_NOT_ENOUGH_TOKEN_MESSAGE: str = "*토큰이 충분하지 않아 이미지를 생성할 수 없습니다.\n*토큰은 구독권 결제일에 달에 한 번(15개) 충전됩니다."
+USER_HAS_NOT_ENOUGH_TOKEN_MESSAGE: str = "토큰이 충분하지 않아\n이미지를 생성할 수 없습니다.\n토큰은 구독권 결제일에\n달에 한 번(15개) 충전됩니다."
 CONCURRENT_GENERATION_REQUEST_MESSAGE: str = "현재 요청하신 이미지가 생성 중입니다.\n생성이 완료된 후 다시 요청해주세요."
