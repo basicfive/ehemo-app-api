68dedbb0ccb76252033d43ac682a8cc60921746b
radiantjade <argentum1450@gmail.com>
2024-12-12 18:29:48 +0900

    (temp) 토큰 부족할 경우 에러 발생 멘트 수정 - ios 심의 통과

--- app/core/errors/error_messages.py
+++ app/core/errors/error_messages.py
@@ -1,4 +1,5 @@
-USER_HAS_NOT_ENOUGH_TOKEN_MESSAGE: str = "토큰이 충분하지 않아\n이미지를 생성할 수 없습니다.\n토큰은 구독권 결제일에\n달에 한 번(15개) 충전됩니다."
+# USER_HAS_NOT_ENOUGH_TOKEN_MESSAGE: str = "토큰이 충분하지 않아\n이미지를 생성할 수 없습니다.\n토큰은 구독권 결제일에\n달에 한 번(15개) 충전됩니다."
+USER_HAS_NOT_ENOUGH_TOKEN_MESSAGE: str = "토큰이 충분하지 않아\n이미지를 생성할 수 없습니다.\n토큰은 달에 한 번(15개) 충전됩니다."
 CONCURRENT_GENERATION_REQUEST_MESSAGE: str = "현재 요청하신 이미지가 생성 중입니다.\n생성이 완료된 후 다시 요청해주세요."
 
 INTERNAL_SERVER_ERROR_MESSAGE: str = "서버 에러가 발생했습니다.\n잠시 후에 다시 시도해주세요."
