aacb91fcced8d57015246c96c47c72f454d0f6dc
radiantjade <argentum1450@gmail.com>
2025-05-15 18:24:58 +0900

    (fix) generation request info 반환 시 이미지 denoising strength 값 전달

--- app/application/generation/request/dto/request_info.py
+++ app/application/generation/request/dto/request_info.py
@@ -44,5 +44,6 @@ class GenerationRequestInfo(BaseModel):
     is_user_reference_image: bool
     user_reference_image_s3_key: Optional[str] = None
     user_reference_image_url: Optional[str] = None
+    user_reference_image_denoise_strength: Optional[float] = None
 
     consumed_tokens: int
